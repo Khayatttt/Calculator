8a44fdd3add8b451dee32a75209c36cc6c32d352
Khayatttt <tunggaln30@students.amikom.ac.id>
2023-03-16 14:51:43 +0700

    menambahkan file calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,13 @@ class Calculator
 {
 	static void main (string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		Console.writeline("Hasil Penambahan: {0} + {1} = {2}", a,b, Penambahan (a,b));
+		Console.writeline("Hasil Pengurangan: {0} - {1} = {2}", a,b, Pengurangan (a,b));
 		
+		Console.WriteLine("\nTekan sembarang key untuk keluar")';
+		Console.ReadKey();
 	}
 	
 	static int penambahan (int a, int b)
